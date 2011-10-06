30b48b4b66e4367129c64b71315e449f058dd6f2
William Nelson <bnelson@arabidopsis.org>
2011-10-06 08:51:04 -0700

    switched to realtive paths in blast shell script

--- lib/tasks/blast.rb
+++ lib/tasks/blast.rb
@@ -6,9 +6,9 @@ output_format = ARGV.pop
 expectvalue = ARGV.pop
 timestamp = ARGV.pop
 
-blastCmd = "blastn -query /Users/bnelson/rails_projects/demo_app/tmp/#{timestamp}_query.fa -db \"#{db_list}\" -task blastn -evalue #{expectvalue} -num_alignments 2 -num_descriptions 2 -html -outfmt #{output_format} -out /Users/bnelson/rails_projects/demo_app/tmp/#{timestamp}_working.txt"
+blastCmd = "blastn -query tmp/#{timestamp}_query.fa -db \"#{db_list}\" -task blastn -evalue #{expectvalue} -num_alignments 2 -num_descriptions 2 -html -outfmt #{output_format} -out tmp/#{timestamp}_working.txt"
 puts blastCmd
 
 system(blastCmd)
-system("mv /Users/bnelson/rails_projects/demo_app/tmp/#{timestamp}_working.txt /Users/bnelson/rails_projects/demo_app/tmp/#{timestamp}_result.txt")
+system("mv tmp/#{timestamp}_working.txt tmp/#{timestamp}_result.txt")
 
