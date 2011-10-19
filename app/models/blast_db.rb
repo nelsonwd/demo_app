5ecb3b63d61493e9bc93e72b41cd560d464d5555
William Nelson <bnelson@arabidopsis.org>
2011-10-19 05:01:44 -0700

    tweaked blastDb file loader

--- app/models/blast_db.rb
+++ app/models/blast_db.rb
@@ -1,9 +1,9 @@
 class BlastDb < ActiveRecord::Base
-  def self.run
+  def run
     file = File.open("#{Rails.root}/db/blast_db.csv", 'rb')
     while(line = file.gets)
-      row = line.split(",");
-      BlastDb.create(:id => row[0],:display_name => row[1],:blast_index_name => row[2],:file_name => row[3],:organism_name => row[4],:taxonomy_id => row[5],:description => row[6],:data_type => row[7],:num_seqs => row[8],:url => row[9],:image => row[10],:parent => row[11],:created_at => row[12],:updated_at => row[13])
+      row = line.split("\",\"");
+      BlastDb.create(:display_name => row[1],:blast_index_name => row[2],:file_name => row[3],:organism_name => row[4],:taxonomy_id => row[5],:description => row[6],:data_type => row[7],:num_seqs => row[8],:url => row[9],:image => row[10],:parent => row[11],:created_at => row[12],:updated_at => row[13])
     end
   end
 end
